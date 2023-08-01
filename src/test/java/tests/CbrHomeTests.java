d829d46ef869bae5ba99c30a9c96cf4aacf625e5
Denis Litinskiy <plnvoran@mail.ru>
2023-08-01 10:09:37 +0300

    add youTubePage.cookiePressAcceptAll() 3

--- src/test/java/tests/CbrHomeTests.java
+++ src/test/java/tests/CbrHomeTests.java
@@ -159,7 +159,6 @@ public class CbrHomeTests extends TestBase {
 
         step("Going to Yandex url and check the url", () -> {
             cbrHomePage.checkAllNetWorksItemIsVisible();
-            youTubePage.cookiePressAcceptAll();
             cbrHomePage.checkYoutubeUrlIsRight();
         });
     }
