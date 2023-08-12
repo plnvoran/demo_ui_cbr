77288d4fdfbf7ad70dfb5db2cf25d2fbebbb77a0
Denis Litinskiy <plnvoran@mail.ru>
2023-08-12 09:30:30 +0300

    Delete "system:properties"

--- src/test/java/config/ProjectConfig.java
+++ src/test/java/config/ProjectConfig.java
@@ -3,7 +3,6 @@ package config;
 import org.aeonbits.owner.Config;
 @Config.Sources({
         "classpath:config/${platform}.properties",
-        "system:properties"
 })
 public interface ProjectConfig extends Config {
 
